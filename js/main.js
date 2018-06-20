32948545f39d19877f1232855bc5e8e0438e566c
zapzqc@qq.com <zap19821218>
2018-06-20 19:05:59 +0800

    调整默认显示为全国，修改图层控制样式

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ var markerTool; //加点工具
 
 window.onload = function () {
     var satelliteLayerUrl = "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineCommunity/MapServer/tile/{z}/{y}/{x}";//影像图地址
-    var mapCenter = new T.LngLat(118.13214548600001, 24.424820321000027);//地图中心点坐标
-    var zoom1 = 14;//级别
+    var mapCenter = new T.LngLat(109.69, 34.968);//地图中心点坐标
+    var zoom = 4;//级别
     map = new T.Map('map', {
         minZoom: 3,//地图允许展示的最小级别
         maxZoom: 18,//地图允许展示的最大级别
-        zoom: zoom1,//地图的初始化级别
-        center: mapCenter //地图的初始化中心点
+        // zoom: zoom,//地图的初始化级别
+        // center: mapCenter //地图的初始化中心点
         // projection: "EPSG:900913"
     });
     var gaode = new T.TileLayer("", {
@@ -20,10 +20,10 @@ window.onload = function () {
     // var satelliteLayer = new T.TileLayer(satelliteLayerUrl);
     // map.addLayer(satelliteLayer);
     // map.centerAndZoom(mapCenter, zoom); // 定位到中心点和指定级别
-    var zoom = new T.Control.Zoom();
-    zoom.setPosition("bottomright");
+    var zoomCtrl = new T.Control.Zoom();
+    zoomCtrl.setPosition("bottomright");
     //添加缩放平移控件
-    map.addControl(zoom);
+    map.addControl(zoomCtrl);
     markerTool = new T.MarkTool(map); // 初始化加点工具
 };
 
@@ -52,7 +52,7 @@ function addMarker(data) {
 }
 
 function createInfoWindow(data, layer) {
-    var sContent = "<div style='margin:10px 0 10px 0;color:#0074d9;font-size: 13px'>" +
+    var sContent = "<div style='margin:10px 0 10px 0;color:#0074d9;font-size: 16px'>" +
         layer.name + "</div>";
     if (data.name) {
         sContent += "<div>名称：" + data.name + " </div>";
