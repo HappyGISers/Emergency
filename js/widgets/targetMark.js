90ce40d8d469dac8a6f5258b1479b2515ebf59db
zapzqc@qq.com <zap19821218>
2018-06-20 11:20:30 +0800

    修改样式

--- js/widgets/targetMark.js
+++ js/widgets/targetMark.js
@@ -2,6 +2,7 @@
  * Created by wangbing on 2018/6/8.
  */
 $(function () {
+    //查询图层的列表
     $.ajax({
         type : "POST",
         url : parent.ctx+"/eventMap/queryTabulation.vm",
@@ -15,47 +16,98 @@ $(function () {
         }
     });
 
+    //组合列表dom
     var initCheckbox = function(data){
         var tableContent = "";
+        function opacity(i){
+            var opacity = '';
+            opacity += '<div class="layer-alert-opacity">';
+            opacity += '<div class="left"> <p>不透明</p> <div class="opacity-btn" id="opacity-minus'+i+'"></div> </div>';
+            opacity += '<div class="right"> <p>透明</p> <div class="opacity-btn" id="opacity-add'+i+'"></div> </div>';
+            opacity += '<div class="center"> <span>0%</span> <span>50%</span> <span>100%</span> </div>';
+            opacity += '<div class="scale_panel"> <div class="scale" id="bar'+i+'"> <div class="silderbg"></div> <span id="silder'+i+'"></span> </div> </div>';
+            opacity += '</div>';
+            return opacity;
+        }
         $.each(data,function(i, item){
             item.formIcon = item.formIcon||"../../images/monitor/defMark.png";
-            item.imageUrl= item.imageUrl||"../../images/monitor/defMark.png";
             parent.mainData.data.layerList[item.type] = item;
             tableContent += '<div class="list-layer" >';
             tableContent += '<div class="checkbox" id="'+item.type+'"><img src="'+item.formIcon+'">'+item.name+'</div>';
-            tableContent += '<div class="layer-alert"><div class="btn"></div><div class="layer-alert-menu"></div></div>';
-            tableContent += '</div>';
+            tableContent += '<div class="layer-alert"><div class="btn"></div>';
+            tableContent += '<div class="layer-alert-menu"> <div class="menu-item">透明度</div> </div>';
+            tableContent += opacity(i);
+            tableContent += '</div></div>';
         });
         $("#layersId").html(tableContent);
         addCheckClick();
     }
 
+    //注册事件
     var addCheckClick = function(){
-        $(".checkbox").click(function () {
+        //check点击事件和所对应的透明度事件注册
+        $(".list-layer .checkbox").click(function () {
             var bool = $(this).hasClass('checked');
             if (bool) {
                 $(this).removeClass('checked');
             } else {
                 $(this).addClass('checked');
             }
-            var val = this.id;
-            singleCheckClick(!bool,val);
+            singleCheckClick(!bool,this.id);
+        }).each(function(i){
+            new scale('silder'+i, 'bar'+i, 'opacity-add'+i, 'opacity-minus'+i,this.id);
         });
-        $(".menu-item").click(function () {
-            var bool;
+
+        //菜单点击事件
+        $(".menu-item").click(function (e) {
+            e.stopPropagation();
+            $('.layer-alert-opacity').hide();
             if(this.id=="open"){
-                $('.checkbox').addClass('checked');
-                bool = true;
+                $('.list-layer .checkbox').addClass('checked').each(function(){
+                    singleCheckClick(true,this.id);
+                });
             }else if(this.id=="close"){
-                $('.checkbox').removeClass('checked');
-                bool = false;
+                $('.list-layer .checkbox').removeClass('checked').each(function(){
+                    singleCheckClick(false,this.id);
+                });
+            }else if($(this).html() == '透明度'){
+                $(this).parents('.layer-alert').find('.layer-alert-opacity').show();
+            }else{
+                $('.layer-alert').removeClass('active');
             }
-            $(".checkbox").each(function(){
-                singleCheckClick(bool,this.id);
-            })
         });
+        //弹框
+        $('.layer-alert .btn').click(function (e) {
+            e.stopPropagation();
+            $('.list-layer').css({
+                'z-index': '1'
+            });
+            $(this).parents('.list-layer').css({
+                'z-index': '2'
+            });
+            $('.layer-alert-opacity').hide();
+            if ($(this).parents('.layer-alert').hasClass('active')) {
+                $(this).parents('.layer-alert').removeClass('active');
+            } else {
+                $('.layer-alert').removeClass('active');
+                $(this).parents('.layer-alert').addClass('active');
+            }
+        });
+
+
+        $('.layer-alert-opacity').click(function (e) {
+            e.stopPropagation();
+        });
+        $('body').click(function (e) {
+            e.stopPropagation();
+            if ($('.layer-alert').hasClass('active')) {
+                $('.layer-alert').removeClass('active');
+                $('.layer-alert-opacity').hide();
+            }
+        })
     }
 
+    //点击check后所执行的方法
     var singleCheckClick = function(bool,val){
         var targetMark = parent.mainData.data.targetMark[val];
         if(bool){
@@ -75,6 +127,7 @@ $(function () {
         }
     }
 
+    //查询所对应的列表所有图层
     var queryMarker = function(val){
         $.ajax({
             type : "POST",
@@ -88,7 +141,8 @@ $(function () {
                         if(!targentMark[item.type]){
                             targentMark[item.type] = [];
                         }
-                        targentMark[item.type].push(parent.addMarker(item));
+                        var marker = parent.addMarker(item);
+                        targentMark[item.type].push(marker);
                     })
                 }else{
                 }
