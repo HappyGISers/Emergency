cd054cfeeba06b7b4cb64bdeeabcebfcba00da6f
SYJjuzijun <songyongju210@163.com>
2018-06-23 22:50:54 +0800

    缓冲区doBuffer

--- widgets/buffer/buffer.js
+++ widgets/buffer/buffer.js
@@ -1,15 +1,49 @@
 var T = parent.T;
 var $ = parent.$;
+var ol = parent.ol;
 var map = parent.map;
 var markerTool = parent.markerTool;
 var polylineTool = parent.polylineTool;// 初始化画线工具
 var polygonTool = parent.polygonTool;// 初始化画面工具
 var rectangleTool = parent.rectangleTool;// 初始化矩形工具
+var geoFormat = new ol.format.GeoJSON();
 var currentDrawTool;
+var currentBufferFeature;
+
+var source = new ol.source.Vector({ wrapX: false });
+var bufferLayer = new ol.layer.Vector({
+    source: source,
+    style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: '#00FFFF7D'
+        }),
+        stroke: new ol.style.Stroke({
+            color: "#00FFFF7D",
+            width: 1
+        })
+    })
+});
+
+map.addLayer(bufferLayer);
+
+var drawStyle = new ol.style.Style({
+    fill: new ol.style.Fill({
+        color: '#00ffff7d'
+    }),
+    stroke: new ol.style.Stroke({
+        color: "#00FFFF7D",
+        width: 1
+    }),
+    image: new ol.style.Circle({
+        fill: new ol.style.Fill({
+            color: "#00FFFF7D"
+        })
+    })
+});
 $(function () {
     //获取父窗口的关闭事件
     $('.widgets-select', window.parent.document).find('.widgets-close').click(function () {
-        removeInteractions();
+        clear();
     })
 });
 
@@ -31,21 +65,50 @@ function addGraphic(type) {
 }
 
 function startDraw(drawTool) {
+    map.disableDoubleClickZoom();
     currentDrawTool = drawTool;
     drawTool.open();
-    drawTool.removeEventListener("mouseup", doBuffer.bind(null, drawTool));
-    drawTool.addEventListener("mouseup", doBuffer.bind(null, drawTool));
+    if(drawTool instanceof T.MarkTool)
+    {
+        drawTool.removeEventListener("mouseup", drawMarkEnd);
+        drawTool.addEventListener("mouseup", drawMarkEnd);
+    }
+    else {
+        drawTool.removeEventListener("draw", drawEnd);
+        drawTool.addEventListener("draw", drawEnd);
+    }
 }
 
-function doBuffer(drawTool,currentLnglat, currentMarker, allMarkers) {
-    drawTool.clear();
-    drawTool.close();
+function drawMarkEnd(currentLnglat, currentFeature) {
+    map.enableDoubleClickZoom();
+    doBuffer(currentFeature);
+    closeTool();
 }
 
-function clear() {
+function drawEnd(currentLnglats,area, currentFeature) {
+    map.enableDoubleClickZoom();
+    doBuffer(currentFeature);
+    closeTool();
+}
+
+function doBuffer(currentFeature) {
+    var radius = parseInt($('#bufferRadius').val());
+    var bufferUnit = $('#bufferUnits').val();
+    var geojson = JSON.parse(geoFormat.writeFeature(currentFeature,{dataProjection:map.getView().getProjection().getCode()}));
+    var bufferJson = turf.buffer(geojson, radius, {units: bufferUnit});
+    currentBufferFeature = geoFormat.readFeature(bufferJson);
+    currentBufferFeature.setStyle(drawStyle);
+    source.addFeature(currentBufferFeature)
+}
+
+function closeTool() {
     if(currentDrawTool)
     {
         currentDrawTool.clear();
         currentDrawTool.close();
     }
+}
+function clear() {
+    source.clear();
+    closeTool();
 }
