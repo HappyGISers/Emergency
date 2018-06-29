6bdd98359aa4515e6f526d9b4b7338cd924092f5
zapzqc@qq.com <zap19821218>
2018-06-29 10:44:10 +0800

    修改图片，和获取数据的位置

--- widgets/buffer/buffer.js
+++ widgets/buffer/buffer.js
@@ -1,7 +1,7 @@
 var T = parent.T;
 var ol = parent.ol;
 var map = parent.map;
-var layerList = parent.mainData.data.layerList;
+var layerList = parent.layerList;
 var markerTool = parent.markerTool;
 var polylineTool = parent.polylineTool;// 初始化画线工具
 var polygonTool = parent.polygonTool;// 初始化画面工具
@@ -14,21 +14,20 @@ var bufferResult = {
     count: 0,
     coordinates: [],
     marks: {},
-    contents:{}
+    contents: {}
 };
 
 var currentDrawGeoJson = {
     type: "Feature",
     geometry: null,
-    properties:null
+    properties: null
 };
 
 function initCheckbox() {
-    layerList = parent.mainData.data.layerList;
+    layerList = parent.layerList;
     var tableContent = '';
     var allLayer = document.getElementsByClassName('analog-select-valueall')[0];
-    for(var layer in layerList)
-    {
+    for (var layer in layerList) {
         tableContent += "<p class='checkbox' id=" + layer + ">" + layerList[layer].name + "</p>";
     }
     allLayer.innerHTML = tableContent
@@ -39,8 +38,7 @@ function prepareDraw(type) {
     clearDrawGraphic();
     initBufferResult();
     closeTool();
-    switch (type)
-    {
+    switch (type) {
         case "point":
             startDraw(markerTool);
             break;
@@ -58,8 +56,7 @@ function prepareDraw(type) {
 function startDraw(drawTool) {
     currentDrawTool = drawTool;
     drawTool.open();
-    if(drawTool instanceof T.MarkTool)
-    {
+    if (drawTool instanceof T.MarkTool) {
         drawTool.removeEventListener("mouseup", drawMarkEnd);
         drawTool.addEventListener("mouseup", drawMarkEnd);
     }
@@ -73,12 +70,17 @@ function startDraw(drawTool) {
 //绘制点完成回调
 function drawMarkEnd(currentLnglat, currentFeature) {
     $('.lump').removeClass('active');
+    // var temp=[];
+    // for(var i =0;i<currentLnglat.length;i++)
+    // {
+    //     temp.push(currentLnglat[i].toFixed(6));
+    // }
     currentDrawGeoJson.geometry = getGeometry(currentLnglat, currentFeature);
     closeTool();
 }
 
 //除点以外的绘制完成回调
-function drawEnd(currentLnglats,area, currentFeature) {
+function drawEnd(currentLnglats, area, currentFeature) {
     $('.lump').removeClass('active');
     currentDrawGeoJson.geometry = getGeometry(currentLnglats, currentFeature);
     closeTool();
@@ -95,16 +97,14 @@ function getGeometry(lnglats, feature) {
 
 //开始缓冲并搜索
 function search() {
-    if(currentDrawGeoJson.geometry === null)
-    {
+    if (currentDrawGeoJson.geometry === null) {
         alert('请先绘制缓冲图形');
         return;
     }
     //获取缓冲查询参数设置
     var bufferParameters = getBufferParameters();
     var selectedLayers = bufferParameters.selectedLayers;
-    if(selectedLayers.length === 0)
-    {
+    if (selectedLayers.length === 0) {
         alert('请选择查询对象');
         return;
     }
@@ -115,7 +115,7 @@ function search() {
     //查询结果
     selectedLayers.forEach(function (layer) {
         //获取图层所有数据
-        queryLayer(layer,queryAndAddResult.bind(null,layer,bufferJson));
+        queryLayer(layer, queryAndAddResult.bind(null, layer, bufferJson));
     })
 }
 
@@ -123,21 +123,20 @@ function search() {
 function doBuffer(radius, bufferUnit) {
     var bufferJson = turf.buffer(currentDrawGeoJson,
         radius, {
-        units: bufferUnit,
-            steps:200
-    });
+            units: bufferUnit,
+            steps: 200
+        });
     var bufferCoordinates = [];
-    if(bufferJson && bufferJson.geometry &&
+    if (bufferJson && bufferJson.geometry &&
         bufferJson.geometry.coordinates.length === 1) {
         bufferCoordinates = bufferJson.geometry.coordinates[0];
     }
-    else
-    {
+    else {
         bufferCoordinates = bufferJson.geometry.coordinates
     }
     // //向地图上添加自定义标注
     var points = bufferCoordinates.map(function (coordinate) {
-        return new T.LngLat(coordinate[0],coordinate[1]);
+        return new T.LngLat(coordinate[0], coordinate[1]);
     });
     var polygon = new T.Polygon(points);
     bufferResult.bufferPolygon = polygon;
@@ -145,6 +144,7 @@ function doBuffer(radius, bufferUnit) {
     bufferResult.bufferCoordinates = bufferCoordinates;
     return bufferJson;
 }
+
 function getBufferParameters() {
     var selectedLayers = [];
     $('.analog-select-valueall .checkbox').each(function () {
@@ -152,27 +152,27 @@ function getBufferParameters() {
             selectedLayers.push(this.id);
         }
     });
-    return{
-        radius:parseInt($('#bufferRadius').val()),
-        bufferUnit : $('#bufferUnits').val(),
+    return {
+        radius: parseInt($('#bufferRadius').val()),
+        bufferUnit: $('#bufferUnits').val(),
         selectedLayers: selectedLayers
     }
 }
+
 //查询所对应的列表所有图层
-var queryLayer = function(val, callback){
-    var bufferLayerData = parent.mainData.data.bufferLayerData[val];
-    if(bufferLayerData)
-    {
+var queryLayer = function (val, callback) {
+    var bufferLayerData = parent.targentMark[val];
+    if (bufferLayerData) {
         callback(bufferLayerData);
     }
     else {
         $.ajax({
-            type : "POST",
-            url : parent.ctx+"/eventMap/queryMarker.vm",
-            data:{flag:"point",type:val},
-            dataType:'json',
-            success : function(retMsg){//成功
-                if (retMsg.success){
+            type: "POST",
+            url: parent.ctx + "/eventMap/queryMarker.vm",
+            data: {flag: "point", type: val},
+            dataType: 'json',
+            success: function (retMsg) {//成功
+                if (retMsg.success) {
                     var data = retMsg.data || [];
                     callback(data);
                 }
@@ -182,24 +182,24 @@ var queryLayer = function(val, callback){
 };
 
 //对每个图层进行缓冲查询并添加结果
-function queryAndAddResult(layer,bufferJson,data) {
+function queryAndAddResult(layer, bufferJson, data) {
     var count = 0;
     var list = $('.list-keyword')[0];
-    bufferResult.contents[layer] = bufferResult.contents[layer] ||"";
-    for (var i=0; i<data.length; i++) {
+    bufferResult.contents[layer] = bufferResult.contents[layer] || "";
+    for (var i = 0; i < data.length; i++) {
         var pointData = data[i];
         var coordinate = [parseFloat(pointData.longitude), parseFloat(pointData.latitude)];
         // var coordinate = [pointData.longitude, pointData.latitude];
         //查询当前点是否和缓冲区相交
         var isInPolygon = turf.booleanDisjoint(turf.point(coordinate), bufferJson);
         //如果相交，则添加缓冲结果
-        if(isInPolygon === false) {
+        if (isInPolygon === false) {
             var marker = parent.addMarker(pointData);
             bufferResult.coordinates.push(coordinate);
-            var listContent =getResultContent(layer, pointData.name);
+            var listContent = getResultContent(layer, pointData.name);
             bufferResult.contents[layer] += listContent;
             //添加结果
-            if(!bufferResult.marks[layer]){
+            if (!bufferResult.marks[layer]) {
                 bufferResult.marks[layer] = [];
             }
             bufferResult.marks[layer].push(marker);
@@ -208,31 +208,29 @@ function queryAndAddResult(layer,bufferJson,data) {
         }
     }
     //更新结果列表
-    list.innerHTML +=bufferResult.contents[layer];
+    list.innerHTML += bufferResult.contents[layer];
     //添加标题栏
     var title = document.createElement('p');
-    title.innerHTML = layerList[layer].name + ' (' +count + ') ';
+    title.innerHTML = layerList[layer].name + ' (' + count + ') ';
     document.getElementsByClassName('list-title')[0].appendChild(title);
     $('.list-title .select')[0].innerHTML = '全部 (' + bufferResult.count + ') ';
     var count = Object.keys(bufferResult.contents).length;
-    if(selectLayerCount ===  count)
-    {
+    if (selectLayerCount === count) {
         zoomToBuffer();
     }
 }
 
-function getResultContent(val,name) {
+function getResultContent(val, name) {
     return "<li>\n" +
         "<img src=\"../../" + layerList[val].imageUrl + "\">\n" +
         "<p class=\"listname\">\n" +
-        "<a>"+name+"</a>\n" +
+        "<a>" + name + "</a>\n" +
         "</p>\n" +
         "</li>";
 }
 
 function zoomToBuffer() {
-    if (bufferResult.bufferCoordinates.length)
-    {
+    if (bufferResult.bufferCoordinates.length) {
         map.setViewport(bufferResult.bufferCoordinates);
     }
 }
@@ -247,11 +245,10 @@ function clear(isClearBufferParameters) {
 
 //初始化缓冲区微件
 function initBufferWidget(isClearBufferParameters) {
-    isClearBufferParameters = isClearBufferParameters||true;
+    isClearBufferParameters = isClearBufferParameters || true;
     initBufferResult();
     currentDrawGeoJson.geometry = null;
-    if(isClearBufferParameters)
-    {
+    if (isClearBufferParameters) {
         $('.analog-select-value').html('请选择查询对象');
     }
 }
@@ -259,16 +256,18 @@ function initBufferWidget(isClearBufferParameters) {
 //清空搜索结果
 function initBufferResult() {
     //移除地图上所有缓冲结果点
-    $.each(bufferResult.marks,function (i,layer) {
-        $.each(layer,function (i,mark) {
-            try{
+    $.each(bufferResult.marks, function (i, layer) {
+        $.each(layer, function (i, mark) {
+            try {
                 map.removeOverLay(mark);
-            }catch(e){}
+            } catch (e) {
+            }
         });
     });
-    try{
+    try {
         map.removeOverLay(bufferResult.bufferPolygon);
-    }catch(e){}
+    } catch (e) {
+    }
     bufferResult = {
         bufferJson: {},
         bufferPolygon: {},
@@ -276,20 +275,21 @@ function initBufferResult() {
         bufferCoordinates: [],
         coordinates: [],
         marks: {},
-        contents:{}
+        contents: {}
     };
     $('.list-keyword')[0].innerHTML = '';
-    $('.list-title')[0].innerHTML ='<p class=\"select\">全部（0）</p>';
+    $('.list-title')[0].innerHTML = '<p class=\"select\">全部（0）</p>';
 }
 
 function closeTool() {
-    if(currentDrawTool)
-    {
+    if (currentDrawTool) {
         currentDrawTool.close();
     }
 }
+
 function clearDrawGraphic() {
     try {
         currentDrawTool.clear();
-    }catch (e) {}
+    } catch (e) {
+    }
 }
